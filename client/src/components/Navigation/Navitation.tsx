832331b2a5e490b5c8467f7ce74547c1a0f1670d
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-03-02 14:11:59 +0200

    navbar

--- client/src/components/Navigation/Navitation.tsx
+++ client/src/components/Navigation/Navitation.tsx
@@ -1,51 +1,60 @@
-import { Link } from "react-router-dom";
-import style from "./Navigation.module.scss";
-import { FaSearch, FaUser } from 'react-icons/fa'; 
-import { FiShoppingCart } from 'react-icons/fi'; 
-import { ReactNode } from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import style from './Navigation.module.scss';
+import { FaSearch, FaUser } from 'react-icons/fa';
+import { FiShoppingCart } from 'react-icons/fi';
 
 interface CustomIconProps {
-    className: string;
-    children?: ReactNode;
-  }
-  
-  const CustomIcon: React.FC<CustomIconProps> = ({ className, children }) => (
-    <span className={className}>
-      {children}
-    </span>
-  );
+  className: string;
+  children?: React.ReactNode;
+}
+
+const CustomIcon: React.FC<CustomIconProps> = ({ className, children }) => (
+  <span className={className}>
+    {children}
+  </span>
+);
 
 export const Navitation = () => {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
   return (
-      <header className={style["navbar"]}>
-        <Link to="/">
-          <img src="src/assets/MacStore.png" alt="" className={style['header-logo']} />
-        </Link>
-        <nav className={style["navbar__nav"]}>
-          <div className={style["navbar__products"]}>
-            <ul className={style["navbar__products-ul"]}>
-              <li>Mac</li>
-              <li>Ipad</li>
-              <li>Iphone</li>
-              <li>Watch</li>
-              <li>AirPods</li>
-              <li>Аксесоари</li>
-              <li>Промоции</li>
-            </ul>
-          </div>
-        </nav>
-        <div className={style["navbar__products-icons"]}>
-          <CustomIcon className={style["icon"]}>
-              <FaSearch />
-            </CustomIcon>
-            <CustomIcon className={style["icon"]}>
-              <FiShoppingCart />
-            </CustomIcon>
-            <CustomIcon className={style["icon"]}>
-              <FaUser />
-            </CustomIcon>
-          </div>
-      </header>
+    <header className={style['navbar']}>
+      <Link to="/">
+        <img src="src/assets/Macstore ltd.png" alt="" className={style['header-logo']} />
+      </Link>
+      <div className={style['vertical-line']}></div>
+      <div className={style['header-logo-right']}>
+        <img src="src/assets/mac.png" alt="" />
+      </div>
+      <nav className={style['navbar__nav']}>
+        <div className={style['navbar__products']}>
+          <ul className={style['navbar__products-ul']}>
+            {['Mac', 'Ipad', 'Iphones', 'Watch', 'Airpod', 'Аксесоари', 'Промо'].map((item, index) => (
+              <li
+                key={index}
+                onMouseEnter={() => setHoveredIndex(index)}
+                onMouseLeave={() => setHoveredIndex(null)}
+                className={hoveredIndex !== null && hoveredIndex !== index ? style['is-hovered'] : ''}
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+      <div className={style['navbar__products-icons']}>
+        <CustomIcon className={style['icon']}>
+          <FaSearch />
+        </CustomIcon>
+        <CustomIcon className={style['icon']}>
+          <FiShoppingCart />
+        </CustomIcon>
+        <CustomIcon className={style['icon']}>
+          <FaUser />
+        </CustomIcon>
+      </div>
+    </header>
   );
 };
 
