6b0c29b2dd7addd13487269d655d86cb0dda4c18
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-03-03 20:23:41 +0200

    nav

---
+++ client/src/components/Home/slider/HomeSlider.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import style from './HomeSlider.module.scss'
+
+const HomeSlider = () => {
+  return (
+    <div className={style['apple__multislider']}>
+        <div className=''>
+
+        </div>
+    </div>
+  )
+}
+
+export default HomeSlider
