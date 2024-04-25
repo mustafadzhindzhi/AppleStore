9ea6469ad680b7a21ff14901a10f8dfb278f9839
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-04-25 20:36:07 +0300

    models

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
+const productRouter = require('./router')
 
 app.use(express.json());
 app.use(cors());
@@ -20,9 +21,18 @@ app.get('/', (req, res) => {
 const jwtSecret = 'secret_ecom';
 
 const storage = multer.diskStorage({
-    destination:'./upload/images',
-    filename: (req,file, cb) => {
+    destination: './upload/images',
+    filename: (req, file, cb) => {
         return cb(null, `${file.fieldname}_${Data.now()}${path.extname(file.originalname)}`)
     }
 });
 
+const upload = multer({storage:storage});
+
+app.use('/images', express.static('upload/images'));
+app.post('/upload', upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
---
+++ server/models/Product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    id:{
+        type: Number, 
+        required: true
+    }, 
+    name: {
+        type: String, 
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    new_price: {
+        type: Number, 
+        required: true
+    },
+    old_price: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    available: {
+        type: Boolean,
+        default: true,
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema);
